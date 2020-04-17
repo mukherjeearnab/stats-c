3197b826b398c39239b55b609540987bf982a59c
mukherjeearnab <mukherjeearnab905@gmail.com>
2020-04-17 16:58:35 +0530

    added lxml to req

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     sc.saveG()
-    return send_file('graph.png', mimetype='image/gif')
+    return send_file('graph.png', mimetype='image/png')
 
 if __name__ == '__main__':
     app.run()
