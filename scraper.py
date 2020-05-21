fa80e750976107e977001d0b09fc0ba62fd23afe
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-05-21 08:58:05 +0530

    resolved bugs, added daily recoveries

--- scraper.py
+++ scraper.py
@@ -3,20 +3,31 @@ import requests
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 
+
 def saveG():
     url = 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India'
-    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'})
+    page = requests.get(url, headers={
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'})
 
     soup = BeautifulSoup(page.content, "html.parser")
 
     population = []
-    for span in soup.find_all("span", attrs={"class": "cbs-ibr", "style": "padding:0 0.3em 0 0; width:3.5em"}):
-        population.append(int(span.text.replace(',', '')))
+    index = 0
+    for span in soup.find_all("span", attrs={"class": "cbs-ibr", "style": "padding:0 0.3em 0 0; width:4.55em"}):
+        if index % 2 != 0:
+            index += 1
+            continue
+        elif span.text == '':
+            population.append(0)
+            index += 1
+        else:
+            population.append(int(span.text.replace(',', '')))
+            index += 1
 
     recoveries = []
     flag = 0
     for td in soup.find_all("td", attrs={"class": "bb-lr"}):
-        children = td.findChildren("div" , recursive=False)
+        children = td.findChildren("div", recursive=False)
         if len(children) == 0:
             recoveries.append(0)
         flag = 0
@@ -25,7 +36,7 @@ def saveG():
                 recoveries.append(int(child.get('title', 0)))
                 flag = 1
                 continue
-            elif 'background:Tomato' in child.get('style') and flag == 0 :
+            elif 'background:Tomato' in child.get('style') and flag == 0:
                 recoveries.append(0)
                 continue
             elif flag == 1:
@@ -47,17 +58,27 @@ def saveG():
         diff.append(num - population[index])
         index += 1
 
+    diffr = [recoveries[0]]
+
+    iterpop = iter(recoveries)
+    next(iterpop)
+    index = 0
+    for num in iterpop:
+        diffr.append(num - recoveries[index])
+        index += 1
+
     dates = []
-    for span in soup.find_all("td", attrs={"class": "bb-04em","colspan": "2", "style": "text-align:center"}):
-        dates.append(span.text)#.replace('2020-0', ''))
+    for span in soup.find_all("td", attrs={"class": "bb-04em", "colspan": "2", "style": "text-align:center"}):
+        dates.append(span.text)  # .replace('2020-0', ''))
 
     print(dates, len(dates))
 
     import matplotlib.pyplot as plt
     import seaborn as sns
     sns.set()
-    plt.figure(figsize=(19.20,10.80))
+    plt.figure(figsize=(24.00, 10.80))
     plt.plot(dates, diff, label='New Cases')
+    plt.plot(dates, diffr, label='New Recoveries')
     plt.plot(dates, population, label='Total Cases')
     plt.plot(dates, recoveries, label='Recoveries')
     plt.plot(dates, active, label='Active Cases')
@@ -72,9 +93,9 @@ def saveG():
     # plt.xlabel('Date')
     # plt.xticks(rotation=90)
     # plt.legend()
-    #plt.show()
+    # plt.show()
     plt.savefig('graph.png', format='png')
-    
+
 
 # import pandas as pd
 
