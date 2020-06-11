df554bba87170ba90159ad49eca52e48e9ae7341
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-06-11 12:10:57 +0530

    Resolved Date Bug & increased resolution

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from bs4 import NavigableString
 
 
 def saveG():
-    url = 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India'
+    url = 'https://en.wikipedia.org/wiki/Timeline_of_the_COVID-19_pandemic_in_India'
     page = requests.get(url, headers={
                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'})
 
@@ -85,6 +85,7 @@ def saveG():
 
     dates = []
     for span in soup.find_all("td", attrs={"class": "bb-04em", "colspan": "2", "style": "text-align:center"}):
+        # print(span.text)
         dates.append(span.text)  # .replace('2020-0', ''))
 
     print(dates, len(dates))
@@ -92,7 +93,9 @@ def saveG():
     import matplotlib.pyplot as plt
     import seaborn as sns
     sns.set()
-    plt.figure(figsize=(24.00, 10.80))
+    # plt.tight_layout()
+    # plt.subplots_adjust(left=0.5, right=0.5)
+    plt.figure(figsize=(36.00, 10.80))
     plt.plot(dates, diff, label='New Cases')
     plt.plot(dates, diffr, label='New Recoveries')
     plt.plot(dates, population, label='Total Cases')
@@ -110,7 +113,7 @@ def saveG():
     # plt.xticks(rotation=90)
     # plt.legend()
     # plt.show()
-    plt.savefig('graph.png', format='png')
+    plt.savefig('graph.png', format='png', bbox_inches='tight')
 
 
 # import pandas as pd
